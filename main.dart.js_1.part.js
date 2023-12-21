2831f460dc92b9b2d57bdc3662f35dbd9a33a4a1
Bear <a.9876542@hotmail.com>
2023-12-21 14:10:25 +0800

    test html render

--- main.dart.js_1.part.js
+++
@@ -1,7 +0,0 @@
-self.$__dart_deferred_initializers__=self.$__dart_deferred_initializers__||Object.create(null)
-$__dart_deferred_initializers__.current=function(a,b,c,$){var B={
-b82(){return new A.DM(null)}},A
-B=a.updateHolder(c[3],B)
-A=c[0]
-var z=a.updateTypes([])}
-$__dart_deferred_initializers__["l5IH9Owo7dJbGIMdr2VXhiYRh8I="] = $__dart_deferred_initializers__.current
