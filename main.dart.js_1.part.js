71787a6f4a45140866cd830afd11cfe93d6b29ad
Bear <a.9876542@hotmail.com>
2023-12-21 13:47:05 +0800

    test defered

---
+++ main.dart.js_1.part.js
@@ -0,0 +1,7 @@
+self.$__dart_deferred_initializers__=self.$__dart_deferred_initializers__||Object.create(null)
+$__dart_deferred_initializers__.current=function(a,b,c,$){var B={
+b82(){return new A.DM(null)}},A
+B=a.updateHolder(c[3],B)
+A=c[0]
+var z=a.updateTypes([])}
+$__dart_deferred_initializers__["l5IH9Owo7dJbGIMdr2VXhiYRh8I="] = $__dart_deferred_initializers__.current
