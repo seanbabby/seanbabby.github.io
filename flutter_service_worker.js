d9722c0ae83610dbda781462849ac2d6ce690387
Bear <a.9876542@hotmail.com>
2024-01-02 12:29:53 +0800

    test scroll 3

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "56f24fd90e44b17e54e0681e894612bb",
-"index.html": "1a338519d3c71ec668eccf1e742e4bbf",
-"/": "1a338519d3c71ec668eccf1e742e4bbf",
+"index.html": "97e7b867b055b96d7030a46e4d633588",
+"/": "97e7b867b055b96d7030a46e4d633588",
 "main.dart.js": "d62969216180c257ca3f43dad0fdeee0",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "0779870e0631e939f0c163de78855e77",
