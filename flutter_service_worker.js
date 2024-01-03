23e642ee63ad6dabbf5ee50c25f449e6442ffc35
Bear <a.9876542@hotmail.com>
2024-01-03 14:55:44 +0800

    update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "56f24fd90e44b17e54e0681e894612bb",
-"index.html": "817940d388e8e1a8a7bd4bb4a0f56b94",
-"/": "817940d388e8e1a8a7bd4bb4a0f56b94",
+"index.html": "5ceb1d7755e233a6119fda5e993aed24",
+"/": "5ceb1d7755e233a6119fda5e993aed24",
 "main.dart.js": "8444984321766492e5bbb1ea6e2ba7c8",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "0779870e0631e939f0c163de78855e77",
